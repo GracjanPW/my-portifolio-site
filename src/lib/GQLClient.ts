3ad8d1b195cf684dd557a019fe59f30a8f631dd7
GracjanPW <40919569+GracjanPW@users.noreply.github.com>
2023-09-24 03:00:31 +0100

    05 hard codeded connection string to env variable

--- src/lib/GQLClient.ts
+++ src/lib/GQLClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 const client = new ApolloClient({
-    uri: "http://127.0.0.1:1337/graphql",
+    uri:  `${process.env.NEXT_PUBLIC_STRAPI_API_URL}/graphql`,
     cache: new InMemoryCache()
   });
 
