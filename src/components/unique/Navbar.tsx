efe4ce9478cd9924b39129f7e3ff335368e7423f
GracjanPW <40919569+GracjanPW@users.noreply.github.com>
2023-11-11 15:26:28 +0000

    09 navbar spacing

--- src/components/unique/Navbar.tsx
+++ src/components/unique/Navbar.tsx
@@ -7,12 +7,12 @@ export default function Navbar() {
   
   const path = usePathname()
   return (
-    <nav className='flex md:justify-end justify-center w-[100%] space-x-reverse space-x-6 text-lg text-navbar-text bg-navbar-bg p-4 border-y border-y-black'>
-      <Link href="/" className={path ==="/"?" text-link-active-light font-semibold mr-6":"mr-6"}>Home</Link>
+    <nav className='flex lg:justify-end justify-around w-[100%] lg:space-x-reverse lg:space-x-6 text-lg text-navbar-text bg-navbar-bg p-4 border-y border-y-black '>
+      <Link href="/" className={path ==="/"?" text-link-active-light font-semibold lg:mr-6":"lg:mr-6"}>Home</Link>
       <Link href="/projects" className={/^\/projects/.test(path)?" text-link-active-light font-semibold":""}>Projects</Link>
       <Link href="/blog" className={/^\/blog/.test(path)?" text-link-active-light font-semibold":""}>Blog</Link>
       <Link href="/contact" className={path ==="/contact"?" text-link-active-light font-semibold":""}>Contact</Link>
-      <Link href={`${process.env.NEXT_PUBLIC_STRAPI_API_URL}${process.env.NEXT_PUBLIC_CV_URL}`}>View CV</Link>
+      <Link href={`${process.env.NEXT_PUBLIC_STRAPI_API_URL}${process.env.NEXT_PUBLIC_CV_URL}`} className='whitespace-nowrap'>View CV</Link>
     </nav>
   )
 }
---
+++ src/hooks/useWindows.ts
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react';
+
+function useWindowSize() {
+    if (typeof window === 'undefined') {
+        return {
+            width: 0,
+            height: 0,
+        };
+    }
+    const [windowSize, setWindowSize] = useState({
+        width: window?.innerWidth,
+        height: window?.innerHeight,
+    });
+
+    useEffect(() => {
+        console.log('useWindowSize: useEffect: addEventListener: resize');
+        let timeoutId: NodeJS.Timeout;
+
+        function handleResize() {
+            clearTimeout(timeoutId);
+            timeoutId = setTimeout(() => {
+                setWindowSize({
+                    width: window.innerWidth,
+                    height: window.innerHeight,
+                });
+            }, 100);
+        }
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    return windowSize;
+}
+
+export default useWindowSize
